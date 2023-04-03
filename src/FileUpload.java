0c1e4c4b193bfc1a146859106af434845c365d1b
apimkov <pag2002@gmail.com>
2023-04-03 14:08:19 +0300

    change code

--- src/FileUpload.java
+++ src/FileUpload.java
@@ -1,64 +1,81 @@
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 public class FileUpload {
 
     private static final String CRLF = "\r\n";
     private static final String CHARSET = "UTF-8";
+    private static final Integer MAX_SIZE = 1000000;
     private static final String BOUNDARY = "===apimkov===";
     private static final String HOST = "194.99.21.219";
-    //private static final String HOST = "127.0.0.1";
+    // private static final String HOST = "127.0.0.1";
     private static final String HOST_FILE = " /upload.php ";
 
-    public String httpUpload(byte[] byteStream) throws IOException {
+    public void httpUpload(byte[] byteStream) throws IOException {
         Socket socketClient;
+        Charset UTF8Charset = Charset.forName("UTF8");
         socketClient = new Socket(HOST,80);
-        //socketClient = new Socket(HOST,8080);//"127.0.0.1"
+       // socketClient = new Socket(HOST,8080);//"127.0.0.1"
         OutputStream directOutput = socketClient.getOutputStream();
         PrintWriter body = new PrintWriter(new OutputStreamWriter(directOutput, CHARSET), true);
-        //client send
-        body.append(CRLF);
-        body.flush();
-        addHeader(body, byteStream, BOUNDARY);
-        addImageData(body, directOutput, byteStream, BOUNDARY);
-        addCloseDelimiter(body,BOUNDARY);
+
+        String strImageData = addImageData(body, directOutput, byteStream, BOUNDARY);
+        int lenImage = strImageData.getBytes(UTF8Charset).length;
+        String strDelimeter = addCloseDelimiter(body,BOUNDARY);
+        int lenDelimeter = strDelimeter.getBytes(UTF8Charset).length;
+        int lenContent = lenImage+byteStream.length+lenDelimeter;
+
+        if (lenContent<= MAX_SIZE) {
+            //client send
+            body.append(CRLF);
+            body.flush();
+            addHeader(body, BOUNDARY, lenContent);
+            body.append(strImageData);
+            body.flush();
+            //body.append("BODY");
+            //body.flush();
+            directOutput.write(byteStream);
+            directOutput.flush();
+            body.append(strDelimeter);
+            body.flush();
+        }
+
         //server response
         InputStreamReader in = new InputStreamReader(socketClient.getInputStream());
         BufferedReader bf = new BufferedReader(in);
-        String str = bf.readLine();
-        System.out.println("server: " + str);
+        String serverResponse = null;
+        while ((serverResponse = bf.readLine()) != null)
+            System.out.println("server: " + serverResponse);
+
         socketClient.close();
-        return str;
     }
 
-    private static void addHeader(PrintWriter body, byte[] data,
-                                     final String boundary) {
+    private static void addHeader(PrintWriter body, final String boundary, int lenContent) {
         body.append("POST"+HOST_FILE+"HTTP/1.1");
         body.append(CRLF).append("Host: "+HOST).append(CRLF);
         body.append("Cache-Control: no-cache").append(CRLF);
-        body.append("Content-Length: "+data.length+"").append(CRLF);
-        body.append("Content-Type: "+"multipart/form-data; boundary="+"\""+boundary+"\""+"").append(CRLF);
+        body.append("Content-Length: "+lenContent+"").append(CRLF);
+        body.append("Content-Type: "+"multipart/form-data; boundary="+boundary+"").append(CRLF);
         body.append(CRLF);
         body.flush();
     }
 
 
-    private static void addImageData(PrintWriter body, OutputStream directOutput, byte[] byteStream,
-                                          final String boundary) throws IOException {
-        body.append(boundary).append(CRLF);
-        body.append("Content-Disposition: form-data; inline; filename=\"apimkov.jpg\"").append(CRLF);
-        body.append("Content-Type: image/jpeg").append(CRLF);
-        body.append(CRLF);
-        body.flush();
-        directOutput.write(byteStream);
-        directOutput.flush();
-        body.append(CRLF);
-        body.flush();
+    private String addImageData(PrintWriter body, OutputStream directOutput, byte[] byteStream, final String boundary) {
+        StringBuilder bodyBuilder = new StringBuilder();
+        bodyBuilder.append(boundary).append(CRLF);
+        bodyBuilder.append("Content-Disposition: form-data; inline; filename=\"apimkov.jpg\"").append(CRLF);
+        bodyBuilder.append("Content-Type: image/jpeg").append(CRLF);
+        bodyBuilder.append(CRLF);
+        return bodyBuilder.toString();
+
     }
 
-    private static void addCloseDelimiter(PrintWriter body, final String boundary) {
-        body.append(boundary).append("--").append(CRLF);
-        body.flush();
+    private String addCloseDelimiter(PrintWriter body, final String boundary) {
+        StringBuilder bodyBuilder = new StringBuilder();
+        bodyBuilder.append(CRLF).append(boundary).append("--").append(CRLF);
+        return bodyBuilder.toString();
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,14 @@ import java.io.*;
 
 public class Main {
     private static final String IMAGE_PATH = "C:\\Photo\\apimkov.jpg";
-    private static final Integer IMAGE_MAX_SIZE = 1000000;
 
     public static void main(String[] args) throws IOException {
         File inputFile = new File(IMAGE_PATH);
         BufferedImage image = ImageIO.read(inputFile);
         ByteArrayOutputStream imageStream = new ByteArrayOutputStream();
         ImageIO.write(image,"jpg", imageStream);
-
-        if (imageStream.toByteArray().length <= IMAGE_MAX_SIZE) {
-            FileUpload fileUpload = new FileUpload();
-            fileUpload.httpUpload(imageStream.toByteArray());
-        }
+        FileUpload fileUpload = new FileUpload();
+        fileUpload.httpUpload(imageStream.toByteArray());
     }
 
     }
